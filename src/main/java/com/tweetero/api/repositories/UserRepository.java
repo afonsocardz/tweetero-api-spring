0f5ae86af0122375d23e3f3b8ac451f43015fe49
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-04-04 23:38:03 -0300

    feat: add User repository

---
+++ src/main/java/com/tweetero/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tweetero.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweetero.api.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
