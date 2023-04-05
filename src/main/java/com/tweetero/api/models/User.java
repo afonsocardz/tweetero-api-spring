6ae9edd176c1f0911e35e969f4fd25855d1d76f8
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2023-04-05 11:07:47 -0300

    fix: add @Table to correct use of database

--- src/main/java/com/tweetero/api/models/User.java
+++ src/main/java/com/tweetero/api/models/User.java
@@ -1,16 +1,19 @@
 package com.tweetero.api.models;
 
-import org.springframework.data.annotation.Id;
-
 import com.tweetero.api.dto.UserDTO;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Table(name = "users")
 @NoArgsConstructor
 public class User {
 
@@ -20,6 +23,9 @@ public class User {
   }
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
   @Column(nullable = false)
   private String username;
 
